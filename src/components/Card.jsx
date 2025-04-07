eec442cc25338537436204839abfa598b8d41598
Sijibomi Akande <sijibomia8@gmail.com>
2025-04-07 16:52:42 +0100

    Fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,29 +4,28 @@ import pic from '../assets/images/gpa2.png'
 
 const Card = () => {
   return (
-    <div className = "container-xl lg:container m-auto">
-      <div className = "grid grid-cols-1 md: grid-cols-2 gap-4 p-4 rounded-lg">
-        <div className = "bg-gray-900 p-8 rounded-lg shadow-xl ml-5">
-          <div className = "flex-col flex-shrink-0">
-            <img src = {pic} className = "h-20 w-auto"/>
-            <h2 className = "text-2xl font-bold text-white">Calculate Your GPA</h2>
-            <p className = "mt-4 text-gray-300">Enter your grades and get your CGPA instantly.</p>
-            <div className = "mt-6">
-            </div>
+    <div>
+    <section className = "py-4">
+      <div className = "container-xl lg:container m-auto">
+      <div className = "grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+        <div className = " bg-gray-900 p-8 rounded-lb shadow-md">
+          <img src = {pic} className = "h-20 w-auto"/> 
+          <h2 className="text-2xl font-bold text-white">Calculate Your GPA</h2>
+          <p className="mt-2 mb-4 mt-4 text-gray-300">
+          Enter your grades and get your CGPA instantly
+          </p>
         </div>
-    </div>
-    <div className = "bg-white p-8 rounded-lg shadow-xl mr-5 border border-gray-300">
-        <div className = "flex-col flex-shrink-0">
+        <div className = " bg-white p-8 rounded-lb shadow-md">
           <img src = {logo} className = "h-20 w-auto"/>
-            <h2 className = "text-2xl font-bold text-gray-900">Tailored for Covenant University Students</h2>
-            <p className = "mt-4 text-gray-800">With a just a few clicks get your Entire Course Structure layed out just for you.</p>
-            <div className = "mt-6">
-            </div>
-          </div>
+          <h2 className="text-2xl font-bold text-gray-900">Tailored for Covenant University Students</h2>
+          <p className="mt-2 mb-4 mt-4 text-gray-800">
+          With a just a few clicks get your Entire Course Structure layed out just for you.
+          </p>
         </div>
       </div>
-    </div>
-  
+      </div>
+    </section>
+  </div>
 
   )
 }
--- src/components/Mainbar.jsx
+++ src/components/Mainbar.jsx
@@ -4,30 +4,34 @@ import { NavLink } from 'react-router-dom';
 const Mainbar = () => {
 
   return (
-    <div className = "h-20 w-full grid place-items-center gap-10px bg-gray-900 border border border-gray-300 rounded">
-      <div className = "absolute left-4 top-15 w-auto h-7">
-        <h1 className = "text-white font-bold">CGPA Tracker</h1>
-      </div>
-      <div className = "absolute right-10 top 15 max-w-sm">
-       <div className = "flex h-20 items-center justify-between">
-        <div classNmae = "flex flex-1 items-center justify-center md:items-stretch md:justify-start">
-          <div className = "md:ml-auto">
-            <div className = "flex space-x-2">
-            <NavLink
-            className ="bg-black text-white hover:bg-gray-700 hover:text-black rounded-md px-3 py-2"
-            to = "/">
-              Home
-              </NavLink>
-            <NavLink
-            className = 'bg-black text-white hover:bg-gray-700 hover:text-black rounded-md px-3 py-2'
-            to = "/calculate">
-              Calculate!
-              </NavLink>
+    <div>
+        <nav className="bg-gray-900 border-b border-indigo-700">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="flex h-20 items-center justify-between">
+          <div
+            className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
+          >
+              <span className="hidden md:block text-white text-2xl font-bold ml-2"
+                >CGPA Tracker</span
+              >
+            <div className="md:ml-auto">
+              <div className="flex space-x-2">
+                <NavLink
+                  to="/"
+                  className="bg-black text-white hover:bg-gray-700 hover:text-black rounded-md px-3 py-2"
+                  >Home</NavLink
+                >
+                <NavLink
+                  to="/calculate"
+                  className="bg-black text-white hover:bg-gray-700 hover:text-black rounded-md px-3 py-2"
+                  >Calculate</NavLink
+                >
+              </div>
             </div>
           </div>
         </div>
       </div>
-      </div>
+    </nav>
     </div>
   );
 };
