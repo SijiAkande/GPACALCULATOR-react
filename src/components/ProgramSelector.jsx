1852ef5997915ea800db0f9838a183d02d1230a2
SijiAkande <168214200+SijiAkande@users.noreply.github.com>
2025-04-24 00:15:14 +0100

    Update ProgramSelector.jsx

--- src/components/ProgramSelector.jsx
+++ src/components/ProgramSelector.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ProgramSelector = ({ selectedCollege, handleProgramChange }) => {
   return (
-    <div className = "flex flex-1 items-center justify-center md:items-stretch md:justify-start mt-4">
+    <div className = "flex flex-1 items-center justify-center md:items-stretchmd:justify-start mt-4">
       <label className = "text-1xl text-white flex flex-col">
         Select Program:
         <select onChange={handleProgramChange} className = "group relative border border-gray-300 bg-gray-150 text-gray-500 text-lg px-3 py-1 rounded">
