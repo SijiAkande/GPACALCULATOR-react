0a7e5b81d6f6864de340dc37ab7b6b68011e7bea
SijiAkande <168214200+SijiAkande@users.noreply.github.com>
2025-04-23 23:58:42 +0100

    Update ProgramSelector.jsx

--- src/components/ProgramSelector.jsx
+++ src/components/ProgramSelector.jsx
@@ -2,18 +2,16 @@ import React from 'react';
 
 const ProgramSelector = ({ selectedCollege, handleProgramChange }) => {
   return (
-    <div className = "flex flex-1 items-center justify-center md:items-stretchmd:justify-start mt-4">
+    <div className = "flex flex-1 items-center justify-center md:items-stretch md:justify-start mt-4">
       <label className = "text-1xl text-white flex flex-col">
         Select Program:
         <select onChange={handleProgramChange} className = "group relative border border-gray-300 bg-gray-150 text-gray-500 text-lg px-3 py-1 rounded">
-          <div className = "absolute top-full right-0 rounded-lg p-3 mt-1 shadow-md scale-y-0 group-focus:scale-y-100 origin-top duration-200">
           <option className = "active">Select a program</option>
           {selectedCollege?.programs.map(p => (
             <option key={p.program} value={p.program}>
               {p.program}
             </option>
           ))}
-          </div>
         </select>
       </label>
     </div>
